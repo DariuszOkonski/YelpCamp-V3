a089a1f54d985baa51f8fad518267b501192e522
Dariusz Okoński <darek200180@gmail.com>
2024-10-16 20:44:35 +0200

    add index for seed

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
@@ -9,17 +10,29 @@ db.once('open', () => {
   console.log('Database connected for seed...');
 });
 
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
 const seeDB = async () => {
   await Campground.deleteMany({});
-  // const c = new Campground({ title: 'purple field' });
-  // await c.save();
 
   for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random * cities.length);
-    new Campground({
-      location: `${cities[random1000].city}, ${cities[random].state}`,
+    const random1000 = Math.floor(Math.random() * cities.length);
+
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
     });
+
+    await camp.save();
   }
 };
 
-seeDB();
+seeDB()
+  .then(() => {
+    console.log('Seeded...');
+  })
+  .catch((e) => console.log('Error occured...'))
+  .finally(() => {
+    mongoose.connection.close();
+    console.log('Database connection closed...');
+  });

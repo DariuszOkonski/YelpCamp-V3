939f0b0bd80cf00487f8c6cecd4af35d61dd61de
Dariusz Okoński <darek200180@gmail.com>
2024-12-18 19:51:31 +0100

    add review

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const ExpressError = require('./utils/ExpressError');
 
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
+// const req = require('express/lib/request.js');
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 const db = mongoose.connection;
@@ -104,6 +106,18 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+  })
+);
+
+app.post(
+  '/campgrounds/:id/reviews',
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
